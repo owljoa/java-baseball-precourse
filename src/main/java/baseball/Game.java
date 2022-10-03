3b3497176fc9c71f2ec60ad9a84b0c05062966aa
owljoa <needshield@gmail.com>
2022-10-03 15:39:21 +0900

    feat(게임): 판정 기능 추가
    세자릿수(Numbers)의 판정 기능으로 알 수 있는 스트라이크, 볼 카운트로 각 판정결과에 적합한 메시지 생성 및 반환하는 기능

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class Game {
 
+    private static final int WINNING_STRIKE_COUNT = 3;
+    private static final String WHITE_SPACE = " ";
+    private static final String NOTHING = "낫싱";
+
     private boolean isExit;
     private boolean isSuspended;
     private final Player defender;
@@ -37,5 +41,60 @@ public class Game {
 
     public void enterChallengerNumber(List<Integer> integers) {
         challenger.enterNumbers(integers);
+    }
+
+    public String judgeAndGetJudgementResultMessage() {
+        Judgement judgement = judge();
+        return getJudgementResultMessage(judgement);
+    }
+
+    private Judgement judge() {
+        Judgement judgement = defender.getNumbers().judgeNumbers(challenger.getNumbers());
+
+        if (judgement.getStrikeCount() == WINNING_STRIKE_COUNT) {
+            suspend();
+        }
+
+        return judgement;
+    }
+
+    private void suspend() {
+        this.isSuspended = true;
+    }
+
+    private String getJudgementResultMessage(Judgement judgement) {
+        if (judgement.getStrikeAndBallCount() == 0) {
+            return NOTHING;
+        }
+
+        return makeBallAndStrikeJudgementString(judgement);
+    }
+
+    private String makeBallAndStrikeJudgementString(Judgement judgement) {
+        if (judgement.getStrikeCount() == WINNING_STRIKE_COUNT) {
+            String winningMessage = String.format("%n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return makeStrikeJudgementString(judgement) + winningMessage;
+        }
+        return makeBallJudgementString(judgement) + makeStrikeJudgementString(judgement);
+    }
+
+    private String makeBallJudgementString(Judgement judgement) {
+        if (judgement.getBallCount() == 0) {
+            return "";
+        }
+
+        return String.format("%d볼", judgement.getBallCount());
+    }
+
+    private String makeStrikeJudgementString(Judgement judgement) {
+        if (judgement.getStrikeCount() == 0) {
+            return "";
+        }
+
+        if (judgement.getStrikeCount() > 0 && judgement.getBallCount() == 0) {
+            return String.format("%d스트라이크", judgement.getStrikeCount());
+        }
+
+        return String.format("%s%d스트라이크", WHITE_SPACE, judgement.getStrikeCount());
     }
 }
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,8 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.ArgumentMatchers;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class GameTest {
 
@@ -46,5 +55,50 @@ class GameTest {
         // then
         Assertions.assertTrue(game.isExit());
         Assertions.assertFalse(game.isSuspended());
+    }
+
+    @ParameterizedTest(name = "도전자 플레이어가 숫자 {0}{1}{2}를 입력하면, 판정 메시지 [{3}]을 출력")
+    @CsvSource(
+            value = {
+                    "1:2:3:'3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료'",
+                    "1:4:3:2스트라이크",
+                    "1:3:4:1볼 1스트라이크",
+                    "2:3:5:2볼",
+                    "5:6:7:낫싱",
+            },
+            delimiter = ':')
+    void judgeAndGetJudgementResultMessage_returnJudgementResultMessage(int number1, int number2, int number3, String expected) {
+        // given
+        Game game = new Game();
+        List<Integer> challengerNumber = generateIntegerList(number1, number2, number3);
+        game.enterChallengerNumber(challengerNumber);
+
+        // when
+        String judgementResultMessage = doJudgeAndGetJudgementResultMessage(game);
+
+        // then
+        Assertions.assertEquals(expected, judgementResultMessage);
+    }
+
+    private List<Integer> generateIntegerList(int number1, int number2, int number3) {
+        List<Integer> challengerNumber = new ArrayList<>();
+        challengerNumber.add(number1);
+        challengerNumber.add(number2);
+        challengerNumber.add(number3);
+
+        return challengerNumber;
+    }
+
+    private String doJudgeAndGetJudgementResultMessage(Game game) {
+        MockedStatic.Verification verification = () -> Randoms.pickNumberInRange(
+                ArgumentMatchers.anyInt(), ArgumentMatchers.anyInt()
+        );
+
+        try (final MockedStatic<Randoms> mock = Mockito.mockStatic(Randoms.class)) {
+            mock.when(verification).thenReturn(1, 2, 3);
+            game.start();
+
+            return game.judgeAndGetJudgementResultMessage();
+        }
     }
 }
